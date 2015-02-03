e10c8e6680212d97bcef665ec03adbc6cbf3aa48
HejdesGit <hejdes@gmail.com>
2015-02-03 20:54:18 +0100

    gonogo

--- app/scripts/timer.js
+++ app/scripts/timer.js
@@ -13,11 +13,16 @@ module.exports = (function () {
 
   var config = {
     urls: 'sounds/Knight.mp3',
-    time: {hours: 9, minutes: 30}
+    urls2: 'sounds/GoNoGo.mp3',
+    time: {hours: 9, minutes: 30},
+    time2: {hours: 10, minutes: 59}
   };
 
   var sound = new Howl({
     urls: [config.urls]
+  });
+  var sound2 = new Howl({
+    urls: [config.urls2]
   });
 
   function Init() {
@@ -26,12 +31,22 @@ module.exports = (function () {
     if (millisTillStandUp < 0) {
       millisTillStandUp += 86400000; // it's after stand up, try tomorrow.
     }
+    var millisTillGoNoGO = new Date(now.getFullYear(), now.getMonth(), now.getDate(), config.time2.hours, config.time2.minutes, 0, 0) - now;
+    if (millisTillGoNoGO < 0) {
+      millisTillGoNoGO += 86400000; // it's after stand up, try tomorrow.
+    }
 
     setTimeout(function () {
       console.log("play sound");
       sound.play();
       Init();
     }, millisTillStandUp);
+
+    setTimeout(function () {
+      console.log("play sound");
+      sound2.play();
+      Init();
+    }, millisTillGoNoGO);
   }
 
   return {
