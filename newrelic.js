9225b166dd03b155121ed76ccaf74bf0b9576fdd
Johan Hejde <johan.hejde@netlight.com>
2015-02-05 21:34:09 +0100

    ca

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+ulp/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['DreamTeam'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '625229475489cefb1a43e0c3fc6fe6ae0fd4ad82',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
